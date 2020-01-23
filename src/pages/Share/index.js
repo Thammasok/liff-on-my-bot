94219f2817337ddc8420a27f12be979d9d43c275
Jaranchai <jaranchai.nt@hotmail.com>
2020-01-23 07:41:52 +0700

    ...

--- src/pages/Share/index.js
+++ src/pages/Share/index.js
@@ -15,6 +15,9 @@ const Index = () => {
       </Helmet>
 
       <h1>Test</h1>
+      <div>
+        <img src="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" alt="text" />
+      </div>
     </div>
   )
 }
