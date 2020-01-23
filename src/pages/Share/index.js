abe56feca06ed699378a7b6fc31ee00f37d4416e
Jaranchai <jaranchai.nt@hotmail.com>
2020-01-23 10:18:49 +0700

    ...

--- src/pages/Share/index.js
+++ src/pages/Share/index.js
@@ -7,17 +7,17 @@ const Index = () => {
     <div className="container">
       <Helmet>
       <meta property="fb:app_id" content="1552358151700561" />
-          <meta property="og:url" content="https://eze-line-bot.herokuapp.com/share" />
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content="คุณเป็นใคร ในโลก (ไร้) บาลานซ์" />
-          <meta property="og:description" content="asdasdasdada" />
-          <meta property="og:image" content="https://imgcache.joox.com/music/joox/photo_th_th/toplist_300/6/4/afc30e41e9c3e145adcc4dd4cba6e264.jpg" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="" data-react-helmet="true" />
-          <meta name="twitter:creator" content="" data-react-helmet="true" />
-          <meta name="twitter:title" content="ฟังเพลง Thailand Top 100 - ชาร์ตเพลงฮิต 2020 | JOOX" />
-          <meta name="twitter:description" content="ฟังเพลงล่าสุดบนชาร์ตเพลง Thailand Top 100 บน JOOX รักได้ป่าว ของ Gavin D" />
-          <meta name="twitter:image" content="https://imgcache.joox.com/music/joox/photo_th_th/toplist_300/6/4/afc30e41e9c3e145adcc4dd4cba6e264.jpg" />
+        <meta property="og:url" content="https://eze-line-bot.herokuapp.com/share" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="คุณเป็นใคร ในโลก (ไร้) บาลานซ์" />
+        <meta property="og:description" content="asdasdasdada" />
+        <meta property="og:image" content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" data-react-helmet="true" />
+        <meta name="twitter:creator" content="" data-react-helmet="true" />
+        <meta name="twitter:title" content="ฟังเพลง Thailand Top 100 - ชาร์ตเพลงฮิต 2020 | JOOX" />
+        <meta name="twitter:description" content="ฟังเพลงล่าสุดบนชาร์ตเพลง Thailand Top 100 บน JOOX รักได้ป่าว ของ Gavin D" />
+        <meta name="twitter:image" content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
       </Helmet>
 
       <h1>Test</h1>
