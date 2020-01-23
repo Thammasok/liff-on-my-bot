648105abc208d210bac10a572f00136070aeb24a
Jaranchai <jaranchai.nt@hotmail.com>
2020-01-23 07:54:25 +0700

    333

--- src/pages/Share/index.js
+++ src/pages/Share/index.js
@@ -7,7 +7,8 @@ const Index = () => {
     <div className="container">
       <Helmet>
         <meta property="og:url" content="https://eze-liff.herokuapp.com/" />
-        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="1552358151700561" /> 
+        <meta property="og:type" content="article" /> 
         <meta property="og:title" content="ทดสอบ" />
         <meta property="og:description" content="ต่อยวัดพลังหมัดคัดเลือกห้องเรียน ติดตามตอนต่อๆไปได้ที่เพจแฟนซับนะครับ" />
         <meta property="og:image" content="https://images.pexels.com/photos/3375493/pexels-photo-3375493.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
