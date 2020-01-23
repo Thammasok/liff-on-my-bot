79e87096110d668bb5dd55386da8a338fc31a47a
Jaranchai-mac <jaranchai.nt@hotmail.com>
2020-01-23 12:33:36 +0700

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,12 +22,18 @@ const Index = () => {
   return (
     <div className="container">
       <Helmet>
-        <meta property="og:url" content="https://eze-liff.herokuapp.com/" />
+        <meta property="fb:app_id" content="1552358151700561" />
+        <meta property="og:url" content="https://eze-liff.herokuapp.com/share/share" />
         <meta property="og:type" content="article" />
-        <meta property="og:title" content="ทดสอบ" />
-        <meta property="og:description" content="ต่อยวัดพลังหมัดคัดเลือกห้องเรียน ติดตามตอนต่อๆไปได้ที่เพจแฟนซับนะครับ" />
-        <meta property="og:image" content="https://images.pexels.com/photos/3375493/pexels-photo-3375493.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+        <meta property="og:title" content="คุณเป็นใคร ในโลก (ไร้) บาลานซ์" />
+        <meta property="og:description" content="asdasdasdada" />
+        <meta property="og:image" content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" data-react-helmet="true" />
+        <meta name="twitter:creator" content="" data-react-helmet="true" />
+        <meta name="twitter:title" content="ฟังเพลง Thailand Top 100 - ชาร์ตเพลงฮิต 2020 | JOOX" />
+        <meta name="twitter:description" content="ฟังเพลงล่าสุดบนชาร์ตเพลง Thailand Top 100 บน JOOX รักได้ป่าว ของ Gavin D" />
+        <meta name="twitter:image" content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
       </Helmet>
 
       <FacebookShareButton
