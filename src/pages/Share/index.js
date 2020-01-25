a901ba8b99a9c76a670aac263dcf17a1c59a0cc6
Jaranchai <jaranchai.nt@hotmail.com>
2020-01-25 09:07:30 +0700

    test helmet

--- src/pages/Share/index.js
+++ src/pages/Share/index.js
@@ -1,16 +1,29 @@
 import React from 'react'
-
+import Helmet from 'react-helmet'
 
 const Index = () => {
-  document.getElementsByName("og:url")[0].content = 'https://eze-liff.herokuapp.com/share'
-  document.getElementsByName("og:title")[0].content = 'ชีวิตคือการเดินทาง'
-  document.getElementsByName("og:description")[0].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
-  document.getElementsByName("og:image")[0].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-  document.getElementsByName("twitter:title")[0].content = 'ชีวิตคือการเดินทาง'
-  document.getElementsByName("twitter:description")[0].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
-  document.getElementsByName("twitter:image")[0].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  // document.getElementsByTagName('meta')[4].content = 'https://eze-liff.herokuapp.com/share'
+  // document.getElementsByTagName('meta')[6].content = 'ชีวิตคือการเดินทาง'
+  // document.getElementsByTagName('meta')[7].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
+  // document.getElementsByTagName('meta')[8].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  // document.getElementsByTagName('meta')[12].content = 'ชีวิตคือการเดินทาง'
+  // document.getElementsByTagName('meta')[13].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
+  // document.getElementsByTagName('meta')[14].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
   return (
     <div className="container">
+      <Helmet>
+        <meta property="og:url" content="https://eze-liff.herokuapp.com/share" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="ชีวิตคือการเดินทาง" />
+        <meta property="og:description" content="อุปสรรคคือสิ่งที่ต้องพบเจอ ทำมานานแล้วยังไม่ได้สักทั" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta name="twitter:title" content="ชีวิตคือการเดินทาง" />
+        <meta name="twitter:description" content="อุปสรรคคือสิ่งที่ต้องพบเจอ ทำมานานแล้วยังไม่ได้สักทั" />
+        <meta name="twitter:image" content="https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+      </Helmet>
       <h1>Test</h1>
       <div>
         <img src="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" alt="text" />
