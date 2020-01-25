6cdcaeadc29a6ffd9d148ace8ad5000c821ffb11
Jaranchai <jaranchai.nt@hotmail.com>
2020-01-25 08:52:26 +0700

    change

--- src/pages/Share/index.js
+++ src/pages/Share/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 
 const Index = () => {
-  document.getElementsByTagName('meta')[4].content = 'https://eze-liff.herokuapp.com/share'
-  document.getElementsByTagName('meta')[6].content = 'ชีวิตคือการเดินทาง'
-  document.getElementsByTagName('meta')[7].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
-  document.getElementsByTagName('meta')[8].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-  document.getElementsByTagName('meta')[12].content = 'ชีวิตคือการเดินทาง'
-  document.getElementsByTagName('meta')[13].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
-  document.getElementsByTagName('meta')[14].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  document.getElementsByName("og:url")[0].content = 'https://eze-liff.herokuapp.com/share'
+  document.getElementsByName("og:title")[0].content = 'ชีวิตคือการเดินทาง'
+  document.getElementsByName("og:description")[0].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
+  document.getElementsByName("og:image")[0].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  document.getElementsByName("twitter:title")[0].content = 'ชีวิตคือการเดินทาง'
+  document.getElementsByName("twitter:description")[0].content = 'อุปสรรคคือสิ่งที่ต้องพบเจอ อุปสรรคคือสิ่งที่ต้องพบเจอ'
+  document.getElementsByName("twitter:image")[0].content = 'https://images.unsplash.com/photo-1579653125967-df28fcdc2eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
   return (
     <div className="container">
       <h1>Test</h1>
